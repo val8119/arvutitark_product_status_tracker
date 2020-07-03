f308585076088caa13c75137260f6cdeac7c65ab
val8119 <mikk.vahaste@gmail.com>
2020-07-03 18:05:46 +0300

    Removed popup.py, integrated into main app.py

--- app.py
+++ app.py
@@ -1,33 +1,50 @@
 import requests
+import time
+import datetime
 from bs4 import BeautifulSoup
 import tkinter
 from tkinter import messagebox
 
-# scraper setup
-total_pages = 5
-url_num = 1
-url_num_str = str(url_num)
-url = f"https://theninenine.com/quotes/top/{url_num_str}/"
+
+hours = 0
+
+url = "https://arvutitark.ee/est/tracking/ER34AFJTTT"
+
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"}
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"}
 
-print("STARTED SCRAPING..")
+now = datetime.datetime.now()
+date_time = now.strftime("%Y-%m-%d %H:%M:%S")
 
-while url_num != total_pages + 1:
-    url_num_str = str(url_num)
-    url = f"https://theninenine.com/quotes/top/{url_num_str}/"
+print(
+    f"\nStarted tracking the status at {date_time}\nURL: https://arvutitark.ee/est/tracking/ER34AFJTTT\n")
 
+
+def check_status():
     page = requests.get(url, headers=headers)
+
     soup = BeautifulSoup(page.content, "html.parser")
 
-    print(f"ON PAGE {url_num_str}/{total_pages}")
+    status = soup.find_all("td", valign="top")[2]
 
-    # save quotes to quotes.py
-    for quote_div in soup.find_all("div", class_="quotesBody"):
-        quote = quote_div.find("p").text
-        if len(quote) <= 280:
-            print(f"'''{quote}''',", file=open("./quotes.py", "a"))
+    print(f"CHECKING STATUS..")
 
-    url_num += 1
+    if status != "Tellimatta":
+        print(f"PRODUCT STATUS HAS CHANGED")
+        show_popup()
 
-print("FINISHED SCRAPING")
+
+def show_popup():
+    root = tkinter.Tk()
+    root.withdraw()
+
+    messagebox.showinfo("Arvutitark", "Product status has changed!")
+
+
+while True:
+    now = datetime.datetime.now()
+    date_time = now.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Checked price at {date_time} ({hours} hours)")
+    check_status()
+    time.sleep(3600)
+    hours += 1
--- popup.py
+++
@@ -1,7 +0,0 @@
-import tkinter
-from tkinter import messagebox
-
-root = tkinter.Tk()
-root.withdraw()
-
-messagebox.showinfo("Arvutitark", "Product status has changed!")
